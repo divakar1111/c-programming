8bc2c7fc8ff903d32a61051798b5880b7dfc527f
divakar1111 <34648294+divakar1111@users.noreply.github.com>
2017-12-19 16:42:39 +0530

    Update b.c

--- codekata/b.c
+++ codekata/b.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main(void) {
+int a;
+{
+scanf("%d",&a);
+if (a>0)
+printf("the number is positive");
+else if (a<0)
+printf("the number is negative");
+else 
+printf("the number is zero");
+}}
